3c91e0167b26f24f7ff28df5baa6b126a1508523
hargun0360 <hargunsingh0720@gmail.com>
2023-10-30 04:37:12 +0530

    Add: Custom Spinner & Integrate API

---
+++ src/Components/CustomSpinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CustomSpinner = () => {
+  return (
+    <div className="spinner-backdrop">
+      <div className="spinner"></div>
+    </div>
+  );
+}
+
+export default CustomSpinner;
--- src/Page/Dashboard.js
+++ src/Page/Dashboard.js
@@ -1,17 +1,44 @@
-import React from 'react'
-import Card from '../Components/Card';
-import Navbar from '../Components/Navbar';
+import React, { useEffect, useState } from "react";
+import Card from "../Components/Card";
+import Navbar from "../Components/Navbar";
+import CustomSpinner from '../Components/CustomSpinner';
 
 const Dashboard = () => {
-  return (<>
-    <div>
+  const [cardData, setCardData] = useState([]);
+  const [userData, setUserData] = useState([]);
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      setIsLoading(true);
+      const response = await fetch("https://api.quicksell.co/v1/internal/frontend-assignment");
+      const data = await response.json();
+      setIsLoading(false);
+      console.log(data);
+      setData(data);
+      setCardData(data.tickets);
+      setUserData(data.users);
+    } catch (e) {
+      console.log(e);
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <div>
         <Navbar />
-        <div className='Card-Container'>
-        <Card />
+        <div className="Dashboard-Container">
+            {isLoading ? <CustomSpinner /> : null}
         </div>
+      </div>
+    </>
+  );
+};
 
-    </div>
-    </>);
-}
-
-export default Dashboard
+export default Dashboard;
