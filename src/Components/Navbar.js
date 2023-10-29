ff429fb889ac7236c832665319c24cb929501469
hargun0360 <hargunsingh0720@gmail.com>
2023-10-30 03:50:02 +0530

    Add: Display Button at Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Navbar = () => {
-  return (<>
-        <div className='Navbar'>
+  const [isOpen, setIsOpen] = useState(false);
 
-        </div>
-    </>)
-}
+  return (
+    <div className="Navbar">
+      <div className="dropdown-container">
+        <button onClick={() => setIsOpen(!isOpen)} className="dropdown-btn">
+          <i className="bx bx-slider"></i>
+          <div className="btn-txt">Display</div>
+          <i className="bx bx-chevron-down"></i>
+        </button>
+        {isOpen && (
+          <div className="dropdown-content">
+            <div className="Grouping">
+              <label>Grouping</label>
+              <select>
+                <option value="Status">Status</option>
+                <option value="User">User</option>
+                <option value="Priority">Priority</option>
+              </select>
+            </div>
+            <div className="Ordering">
+              <label>Ordering</label>
+              <select>
+                <option value="Priority">Priority</option>
+                <option value="User">User</option>
+              </select>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
